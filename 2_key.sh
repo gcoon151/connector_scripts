3d7c85149609b25e400921d7ed9e96635bb76f64
gcoon151 <isptech151@yahoo.com>
2024-04-18 14:49:18 -0400

    Update 2_key.sh

--- 2_key.sh
+++ 2_key.sh
@@ -1,4 +1,5 @@
 !/bin/bash
+# -sso is because I work for the company, you may not need that part
 echo "make sure you're logged in: ibmcloud login -sso"
 ibmcloud iam api-key-create connector-key; 
 echo "now put the string that says 'API Key' in ./env-files/apikey"
