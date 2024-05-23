4aa07ae95d84c7b09b6bcdecff56cfd36f8e339d
gcoon151 <isptech151@yahoo.com>
2024-05-23 11:26:20 -0400

    Update 7_sample_target.sh

--- 7_sample_target.sh
+++ 7_sample_target.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 mkdir webdemo
+# This is the data file served up. 
 echo "I am data " > webdemo/somefile.txt
 cd webdemo
+# The ampersand at the end launches the python command to the background. 
+# "fg" in bash will bring it back for you to ctrl-c it or you can just kill it
+# from the process list
 python3 -m http.server 9000 &
