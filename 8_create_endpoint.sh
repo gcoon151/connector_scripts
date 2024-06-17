77751d57bb87f6a4e7a0cc93af6e01f5e7a6963d
gcoon151 <isptech151@yahoo.com>
2024-06-17 15:47:11 -0400

    Update 8_create_endpoint.sh
    fixing create endpoint script.

--- 8_create_endpoint.sh
+++ 8_create_endpoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # GUI has this for the moment https://cloud.ibm.com/satellite/connectors/
-ibmcloud sat endpoint create --name localhostendpoint9k1 --connector-id `sed -n 's/^SATELLITE_CONNECTOR_ID=//p' env-files/env.txt` --source-protocol TCP --dest-hostname localhost --dest-port 9001
+ibmcloud sat endpoint create --dest-hostname localhost --dest-port 9001 --dest-type location --name localhostendpoint9k1 --connector-id `sed -n 's/^SATELLITE_CONNECTOR_ID=//p' env-files/env.txt` --source-protocol TCP
