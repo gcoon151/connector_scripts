72a15e64dcb966abbc5176b7ae822bc12d0b4d84
gcoon151 <isptech151@yahoo.com>
2024-06-17 15:59:50 -0400

    Update 9_curl_connector_endpoint.sh
    Fixing with new endpoint commands.

--- 9_curl_connector_endpoint.sh
+++ 9_curl_connector_endpoint.sh
@@ -2,12 +2,11 @@
 # The resources offered through connector are by default exposed
 # on the IBM private network so it's recommended to use cloud shell to easily
 # get on that network https://cloud.ibm.com/shell
-ibmcloud sat endpoint ls --connector `awk -F= '$1=="SATELLITE_CONNECTOR_ID"{print $2}' ./env-files/env.txt` 
-...add here grab region for next curl command from json output and maybe echo some export $CONNECTORTARGET=VALUE:PORT
+echo "This is the target interface NOTE ONLY REACHABLE from within IBM Cloud"
+ibmcloud sat endpoint get --endpoint localhostendpoint9k1 --connector-id `sed -n 's/^SATELLITE_CONNECTOR_ID=//p' env-files/env.txt`|grep Address|sed -n 's/^.*TCP //p'
 # Take this command to cloud shell if you're not already in it
 # Simple hit TCP port
-curl telnet://c-01.private.us-south.link.satellite.cloud.ibm.com:YOURPORT
-#debug 
-curl -v telnet://c-01.private.us-south.link.satellite.cloud.ibm.com:YOURPORT
+echo "now curl it from https://cloud.ibm.com/shell" 
+echo "Debug like: curl -v telnet://c-01.private.us-south.link.satellite.cloud.ibm.com:YOURPORT"
 # dig command with trace and debug for DNS shenanigans
 # 
