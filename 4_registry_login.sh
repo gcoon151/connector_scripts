69fafe44de7eb9253431f39c1da61b2e1b505826
gcoon151 <isptech151@yahoo.com>
2024-07-02 12:07:16 -0400

    Update 4_registry_login.sh

--- 4_registry_login.sh
+++ 4_registry_login.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Install container registry plugin
 ibmcloud plugin install cr
+# Set global region.
 ibmcloud cr region-set icr.io
+# login to that container registry - obv requires working docker command or change docker to whatever your container platform is. 
+# For rancher I change this to say nerdctl login -u iamapikey --password-stdin icr.io
 cat ./env-files/apikey | docker login -u iamapikey --password-stdin icr.io
