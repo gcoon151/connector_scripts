61cb3445a444392f1152f9ab41b1868eb92c6856
gcoon151 <isptech151@yahoo.com>
2024-07-02 12:08:45 -0400

    Update 6_run_agent.sh

--- 6_run_agent.sh
+++ 6_run_agent.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "Hint if you have ARM mac, use rancher and change this to nerdctl in the script. Rancher works best on ARM macs"; 
 docker run -d --network host --platform linux/amd64 --env-file ~/agent/env-files/env.txt -v ~/agent/env-files:/agent-env-files icr.io/ibm/satellite-connector/satellite-connector-agent:latest
