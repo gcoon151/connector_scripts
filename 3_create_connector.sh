c189dd75c295855ef37e0c956bc2e94179ed7e2c
gcoon151 <isptech151@yahoo.com>
2024-07-02 12:05:18 -0400

    Update 3_create_connector.sh

--- 3_create_connector.sh
+++ 3_create_connector.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 # unfortunately the ibmcloud cli wants a resource group target for the next command to succeed. 
 ibmcloud target -g Default
+echo "If the target command failed, check what resource groups you have and target that.";
 # Make a connector. 
 ibmcloud sat experimental connector create --name connector123 --region us-east
 echo "The connectorID needs to go in ./env-files/env.txt";
