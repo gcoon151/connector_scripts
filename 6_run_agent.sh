f7726abfa53e7cab93bc791a111bea9b047040c6
gcoon151 <isptech151@yahoo.com>
2024-10-16 11:27:45 -0400

    Update 6_run_agent.sh
    add another rancher note

--- 6_run_agent.sh
+++ 6_run_agent.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Hint if you have ARM mac, use rancher and change this to nerdctl in the script. Rancher works best on ARM macs"; 
+# additional notes on rancher I use containerd and don't even start the kubernetes stuff
 docker run -d --network host --platform linux/amd64 --env-file ~/agent/env-files/env.txt -v ~/agent/env-files:/agent-env-files icr.io/ibm/satellite-connector/satellite-connector-agent:latest
