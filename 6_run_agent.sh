1547d74ea6ac3bfd75aeacf8cecaa2597b191892
Gerald Coon <isptech151@yahoo.com>
2025-04-01 09:39:07 -0400

    Update 6_run_agent.sh
    point to unauthenticated container registry image by default just to make things easier for folks.

--- 6_run_agent.sh
+++ 6_run_agent.sh
@@ -2,4 +2,8 @@
 echo "Hint if you have ARM mac, use rancher and change this to nerdctl in the script. Rancher works best on ARM macs"; 
 echo "Now have arm agent images. May not be an issue any more."
 # additional notes on rancher I use containerd and don't even start the kubernetes stuff
-docker run -d --network host --platform linux/amd64 --env-file ~/agent/env-files/env.txt -v ~/agent/env-files:/agent-env-files icr.io/ibm/satellite-connector/satellite-connector-agent:latest
+docker run -d --network host --platform linux/amd64 --env-file ~/agent/env-files/env.txt -v ~/agent/env-files:/agent-env-files us.icr.io/armada-master/satellite-connector-agent:latest
+# Use this one for production: icr.io/ibm/satellite-connector/satellite-connector-agent:latest
+# The one above is unauthenticated by default and just easier for first timers. Best way to authenticate and pull
+# image is with the ibmcloud cr (container registry) plugin. It can configure docker/podman to pull images. 
+# for openshift, you'll need to configure pull secrets. Same as setting up for docker hub authentication. 
