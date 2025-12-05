db90cb7f358482303d3f9d2b6a266ea3c87a7ba9
Gerald Coon <isptech151@yahoo.com>
2025-12-05 15:01:00 -0500

    Replace docker with podman for image pull
    most folks I talk to these days are using podman. Making that the example.

--- 5_pull_image.sh
+++ 5_pull_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # or change docker to nerdctl for rancher
-docker pull us.icr.io/armada-master/satellite-connector-agent:latest
+podman pull us.icr.io/armada-master/satellite-connector-agent:latest
 # I know I should put the public one icr.io/ibm/satellite-connector/satellite-connector-agent:latest 
 # but authentication is such a pain some times and I like things working by default. 
 # The uncommented docker pull command above grabs a version of the agent that doesn't require logging in. 
